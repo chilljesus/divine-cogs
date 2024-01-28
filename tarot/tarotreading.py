54204300a09976abdc5a2b82c2b2c3d346445880
Jesus <neko@jesus.sh>
2024-01-28 02:05:17 -0500

    uh

--- tarot/tarotreading.py
+++ tarot/tarotreading.py
@@ -23,39 +23,41 @@ class TarotReading(commands.Cog):
         return [f.replace('.json', '') for f in os.listdir(cards_path) if f.endswith('.json')]
 
     @commands.hybrid_command(name="tarot", description="Perform a tarot reading")
-    #async def _tarot(self, ctx, deck: str = None, user: discord.Member = None):
-    async def _tarot(self, ctx, *, arg=None):
-        user = ctx.author
-        deck = random.choice(self.list_decks())
-        if arg:
-            args = arg.split()
-            for a in args:
-                try:
-                    # Attempt to convert each argument to a Discord member
-                    potential_user = await commands.MemberConverter().convert(ctx, a)
-                    user = potential_user
-                except BadArgument:
-                    # If not a user, check if it's a deck name
-                    if a in self.list_decks():
-                        deck = a
-        else:
+    #async def _tarot(self, ctx, *, arg=None):
+    #    user = ctx.author
+    #    deck = random.choice(self.list_decks())
+    #    if arg:
+    #        args = arg.split()
+    #        for a in args:
+    #            try:
+    #                # Attempt to convert each argument to a Discord member
+    #                potential_user = await commands.MemberConverter().convert(ctx, a)
+    #                user = potential_user
+    #            except BadArgument:
+    #                # If not a user, check if it's a deck name
+    #                if a in self.list_decks():
+    #                    deck = a
+    #    else:
+    #        deck = random.choice(self.list_decks())
+    #    user = user or ctx.author
+    #    card = await self.get_random_card(deck)
+    async def _tarot(self, ctx, deck: Optional[str] = None, user: Optional[discord.Member] = None):
+        """Performs a tarot reading. Optionally specify a deck and a user."""
+        user = user or ctx.author
+        if deck and deck not in self.list_decks():
             deck = random.choice(self.list_decks())
 
-        user = user or ctx.author
-        
-        #user = user or ctx.author
-        #deck = deck or random.choice(self.list_decks())
-
+        deck = deck or random.choice(self.list_decks())
         card = await self.get_random_card(deck)
-
+        
         embed = discord.Embed(title=card['card_name'],
                       url=card['card_url'],
                       colour=0xffc0cb,
                       timestamp=datetime.now())
 
         embed.set_author(name=f"Reading for {user.display_name}", icon_url=user.display_avatar)
-        embed.add_field(name="Card Description", value=card['card_meaning'], inline=True)
-        embed.add_field(name="Upright Meaning", value=card['upright_meaning'], inline=True)
+        embed.add_field(name="Card Description", value=card['upright_meaning'], inline=True)
+        embed.add_field(name="Upright Meaning", value=card['card_meaning'], inline=True)
         embed.set_thumbnail(url=card['card_image'])
         embed.set_footer(text=f"Deck: {deck}", icon_url="https://nekoism.co/images/logo-small.png")
         
