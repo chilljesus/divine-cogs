2040c9b2c197d50a553b7f3bfa0dab7fa02b873d
Jesus <neko@jesus.sh>
2024-02-03 03:17:18 -0500

    .

--- ollama/ollama.py
+++ ollama/ollama.py
@@ -25,7 +25,7 @@ class Ollama(commands.Cog):
         pass
 
     @ollama.command(name="getmodels")
-    async def sethost(self, ctx):
+    async def sethost(self):
         """Get the available models."""
         api_url = f"http://{await self.config.api_hostname()}:{await self.config.api_port()}/api/tags"
 
