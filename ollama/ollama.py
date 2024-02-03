2393915e41cbbec29d4bd1f8693d2ad8ef19f1a8
Jesus <neko@jesus.sh>
2024-02-03 10:58:18 -0500

    .

--- ollama/ollama.py
+++ ollama/ollama.py
@@ -211,8 +211,8 @@ class Ollama(commands.Cog):
     @ollama.command(name="newchat")
     async def newchat(self, ctx):
         if ctx.guild is not None:
-            thread_name = f"{message.author.display_name} Chat"
-            thread = await message.create_thread(name=thread_name, auto_archive_duration=60)
+            thread_name = f"{ctx.author.display_name} Chat"
+            thread = await ctx.create_thread(name=thread_name, auto_archive_duration=60)
             #async with self.config.guild(ctx.guild).chats() as chats:
             #    if ctx.channel.id not in chats:
             #        chats.append(ctx.channel.id)
