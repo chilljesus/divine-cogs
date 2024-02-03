1996c1146175a57c69f8284d1ea0fb4b7787c9b6
Jesus <neko@jesus.sh>
2024-02-03 14:08:40 -0500

    .

--- ollama/ollama.py
+++ ollama/ollama.py
@@ -276,7 +276,7 @@ class Ollama(commands.Cog):
             threads = False
         chats = await self.config.guild(message.guild).chats()
         #  or ((message.channel.type == "public_thread" or "private_thread") and message.channel.owner.id == self.bot.user.id)
-        if isinstance(message.channel, discord.DMChannel) or (message.channel.id in chats):
+        if isinstance(message.channel, discord.DMChannel) or (message.channel and message.channel.id in chats):
             history = []
             async for msg in message.channel.history(limit=await self.config.history_limit()):
                 if msg.content == "New Chat Initialized.":
